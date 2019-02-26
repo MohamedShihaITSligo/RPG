bbc8c05a9e60e63899c112f6327d73068789a535
MohamedShiha <46888852+MohamedShihaITSligo@users.noreply.github.com>
2019-02-26 22:15:38 +0000

    added the world
    i nedd to fix the spwaner to sowan limited objects

---
+++ Assets/Scripts/MoveRandomly.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRandomly : MonoBehaviour
+{
+    public int speed;
+    Vector2 direction;
+    Rigidbody2D body;
+
+
+    void Start()
+    {
+        speed = 8;
+        body = gameObject.GetComponent<Rigidbody2D>();
+        Move();
+    }
+
+    void Move()
+    {
+        int leftOrRight = Random.Range(-1, 2);
+        if (leftOrRight == 0) leftOrRight = -1;
+        direction.x = Random.Range(-120, 120);
+        direction.y = Random.Range(-120, 120);
+        direction.Normalize();
+        body.velocity = direction * speed * leftOrRight;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Barrier"))
+            body.velocity = body.velocity * -1;
+           // Move();
+    }
+}
--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -12,13 +12,14 @@ public class PlayerData : MonoBehaviour {
         XP,
         Badpoints,
         GoodPoint,
-        xpUntilNextLevel = 10,
+        xpUntilNextLevel = 8,
         Level;
     GameManager manager;
     private void Start()
     {
         manager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
     }
+
     private void Update()
     {
         CheckIfLevelledUp();
@@ -33,24 +34,28 @@ public class PlayerData : MonoBehaviour {
             Speed++;
         }
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         string tag = collision.tag;
         if (tag.Equals("Gas"))
         {
             XP += 10;
+            Fule += 10;
             Destroy(collision.gameObject);
-        }else if (tag.Equals("Human"))
+        }
+        else if (tag.Equals("Human"))
         {
             Badpoints++;
-            Destroy(collision.gameObject);
             manager.SpawnBlood(collision.transform.position);
+            Destroy(collision.gameObject);
         }
-        else if (tag.Equals("Alien"))
+        else if (tag.Equals("Zombi"))
         {
             GoodPoint++;
-            Destroy(collision.gameObject);
+            XP++;
             manager.SpawnBlood(collision.transform.position);
+            Destroy(collision.gameObject);
         }
     }
 }
